644697a981bb0013cb4843aa4144ee65a3bb8267
jjrubio <jefferson.jrubio@gmail.com>
2014-04-15 14:22:48 -0500

    designIndex.css fixed

--- OSEE/settings.py
+++ OSEE/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'OSEEdb',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
