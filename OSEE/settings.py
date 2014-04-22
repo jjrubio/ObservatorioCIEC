131edf31940ad1904aae55d5f131ef86f3a590fc
jjrubio <jefferson.jrubio@gmail.com>
2014-04-22 08:59:38 -0500

    media folder fixed

--- OSEE/settings.py
+++ OSEE/settings.py
@@ -106,9 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "media"),
 ]
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL='/media/'
