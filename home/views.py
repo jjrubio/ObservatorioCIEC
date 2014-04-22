cfcf94d698a5426458658c82a3569cd1e062b149
Andrea <andreacaceresm@gmail.com>
2014-04-22 11:30:54 -0500

    module staff and css

--- home/views.py
+++ home/views.py
@@ -5,11 +5,12 @@ from django.template import Context
 from django.template.context import RequestContext
 from features.models import Description
 from home.models import Slider, Timeline
-
+from staff.models import Personal_data
 
 def home (request):
     description = Description.objects.all()
     imagenes = Slider.objects.all()
+    profile = Personal_data.objects.all()
     template = 'index.html'
     return render_to_response(template, context_instance = RequestContext(request,locals()))
 
