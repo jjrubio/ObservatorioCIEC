b6d97623cbbfdba9a5903eb65be28e44078f3e61
jjrubio <jefferson.jrubio@gmail.com>
2014-05-13 09:20:43 -0500

    static menu - indicators list

--- indicators/views.py
+++ indicators/views.py
@@ -16,10 +16,11 @@ def indicators_detail(request, indicator_id):
 	message = {"ind_name": "", "ind_definition": "", "ind_unit": "", "ind_formula": "", "ind_icon": "", "ind_subcategory": "", "subcategory_icon": "", "ind_category": "", "category_icon": "", }
 	if request.is_ajax():
 		indicator = get_object_or_404(Indicator, id=indicator_id)
+
 		message['ind_name'] = indicator.name
 		message['ind_definition'] = indicator.definition
 		message['ind_unit'] = indicator.unit
-		message['ind_formula'] = indicator.formula_src.path
+		message['ind_formula'] = "/%s" % indicator.formula_src
 		message['ind_icon'] = indicator.icon
 		message['ind_subcategory'] = indicator.subcategory.name
 		message['subcategory_icon'] = indicator.subcategory.icon
--- static/js/basichtml.js
+++ static/js/basichtml.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
         containersToPush: [$( '#pushobj' )],
         menuWidth: '20%',
         backText: 'Atrás',
-
     });
+
+    var $firstIndicator = $( '#menu' ).multilevelpushmenu( 'findmenusbytitle' , 'Toda la población' ).first();
+     $( '#menu' ).multilevelpushmenu( 'expand' , $firstIndicator );
 
     $( window ).resize(function() {
         $( '#menu' ).multilevelpushmenu( 'redraw' );
