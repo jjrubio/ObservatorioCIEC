068eafc7a30c25d11d6fd051c32ee26a70b2921f
unknown <andreacaceresm@gmail.com>
2014-06-14 10:37:55 -0500

    Checkbox filter working

--- static/js/chb.js
+++ static/js/chb.js
@@ -1,41 +1,41 @@
 $(document).ready( function() {
     var limit = 2;
     var count = 0;
-    $('input').on('change', filtro);
-    function filtro(){
-        var id_desagregacion = $(this).val();
-        if (count < limit){
+    $(":checkbox").click(function(){
+        var id = $(this).attr('id');
+        var isChecked = $(this).attr('checked');
+        console.log("id es antes del getJSON:", id);
+        $.getJSON('/list_denied/', {'id_desagregacion': id},
+        function(data){
+            console.log(data);
             $('#ckb').children().remove();
-            $.getJSON('/list_denied/', {'id_desagregacion': id_desagregacion},
-                function(data){
-                    console.log(data);
-                    $.each(data, function(index, item){
-                        //console.log(id_desagregacion);
-                        if(id_desagregacion == item.pk){
-                            //console.log("Pinta el checkbox");
-                            var div='<div class="checkbox">'+
-                                '<label>'+
-                                    '<input type="checkbox" value=\''+item.pk+'\' checked>'+item.fields.name+
-                                '</label>'+
-                            '</div>'
-                            $('#ckb').append(div);
-                            console.log(item.pk);
-                        }else{
-                            //console.log("Muestra los demas");
-                            var div='<div class="checkbox">'+
-                                '<label>'+
-                                    '<input type="checkbox" value=\''+item.pk+'\'>'+item.fields.name+
-                                '</label>'+
-                            '</div>'
-                            $('#ckb').append(div);
-                            console.log(item.pk);
-                        }
-                    });
-                });
-            count++;
-        }else{
-            alert("Solo puede escoger dos desagregaciones.");
-            $(this).prop("checked", "");
-        }
+            $.each(data, function(index, item){
+                if(id == item.pk){
+                    var tmpHTML = "<div class=\'checkbox\'><label><input type=\'checkbox\' checked id="+item.pk+">"+item.fields.name+"</label></div>";
+                    $('#ckb').append(tmpHTML);
+                    console.log(item.pk);
+                }else{
+                    var tmpHTML = "<div class=\'checkbox\'><label><input type=\'checkbox\' id="+item.pk+">"+item.fields.name+"</label></div>";
+                    $('#ckb').append(tmpHTML);
+                    console.log(item.pk);
+                }
+            });
+            count ++;
+            console.log("Valor del contador:",count);
+            validCheckbox();
+        });
+    });
+    
+    function validCheckbox(){
+        $(":checkbox").click(function(){
+            if(count < limit){
+                var id = $(this).attr('id');
+                var isChecked = $(this).attr('checked');
+                count++;  
+            }else{
+                alert("Solo se puede escoger dos desagregaciones.")
+                $(this).prop("checked", "");
+            }
+        });
     }
 });
