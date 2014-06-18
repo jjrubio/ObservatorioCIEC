fe55b30d706133b0a06016cdb9e89a26c05c46c8
unknown <andreacaceresm@gmail.com>
2014-06-17 21:58:03 -0500

    modifiding files

--- OSEE/urls.py
+++ OSEE/urls.py
@@ -6,7 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^$', 'home.views.slider', name='home'),
+    url(r'^slider/', 'home.views.slider', name='slider'),
+    url(r'^team/', 'staff.views.team_group', name='team'),
+    url(r'^features/', 'features.views.features', name='features'),
+    url(r'^registro/', 'registers.views.signup', name='registro'),
+    url(r'^map/', 'home.views.map', name='map'),
     # url(r'^blog/', include('blog.urls')),
     (r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
@@ -17,6 +22,9 @@ urlpatterns = patterns('',
     url(r'^recursos/boletines/', 'resources.views.bulletins', name='bulletins'),
     url(r'^recursos/enlaces-externos/', 'resources.views.links', name='links'),
     url(r'^registro/', 'registers.views.signup', name='registro'),
+    url(r'^list_denied', 'indicators.views.list_by_no_denied', name='list_denied'),
     url(r'^UsuarioCreado/', 'registers.views.signupok', name='ok'),
     url(r'^list_denied', 'indicators.views.list_by_no_denied', name='list_denied'),
+    url(r'^singin/', 'registers.views.signupok', name='login'),
+    url(r'^logout/', 'registers.views.user_logout', name='logout'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -8,24 +8,28 @@ from home.models import Slider, Timeline
 from staff.models import Personal_data
 from registers.forms import UserAuthenticationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
-def home (request):
-    description = Description.objects.all()
+def slider(request):
     imagenes = Slider.objects.all()
-    profile = Personal_data.objects.all()
-    signin = UserAuthenticationForm(request.POST or None)
-    if signin.is_valid():
-    	login(request, signin.get_user())
-
-
-    template = 'index.html'
-
+    template = 'slider.html'
     return render_to_response(template, context_instance = RequestContext(request,locals()))
 
+def map(request):
+    template = 'map.html'
+    return render_to_response(template, context_instance = RequestContext(request,locals()))    
+
+def home (request):
+    signin = UserAuthenticationForm(request.POST or None)
+    if signin.is_valid():
+        login(request, signin.get_user())
+
+    template = 'index.html'
+    return render_to_response(template, context_instance = RequestContext(request,locals()))
 
 def timeline (request):
-	events   = Timeline.objects.all()
-	template = 'timeline.html'
-	return render_to_response(template, context_instance = RequestContext(request,locals()))
+    events   = Timeline.objects.all()
+    template = 'timeline.html'
+    return render_to_response(template, context_instance = RequestContext(request,locals()))
 
 
