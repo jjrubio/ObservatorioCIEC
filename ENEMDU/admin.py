c9dff92cb595bfd593db6074212584a7e0e2a7d5
jjrubio <jefferson.jrubio@gmail.com>
2014-07-15 11:51:30 -0500

    update ENEMDU model

--- ENEMDU/admin.py
+++ ENEMDU/admin.py
@@ -6,12 +6,12 @@ from import_export.admin import ImportExportModelAdmin
 
 class Data_from_year_trimAdmin(ImportExportModelAdmin):
     list_display = (
-        'year', 'trim', 'area', 'region_natural', 'ciudad_ind',
+        'anio', 'trimestre', 'area', 'region_natural', 'ciudad_ind',
         'fexp', 'genero', 'edad', 'etnia', 'edad_group', 'nivinst',
         'anosaprob', 'pet', 'pei', 'pea', 'ocupa', 'onocla', 'oplenos',
         'suboc', 'suboc1', 'suboc2', 'deso', 'deso1', 'deso2',
         'deaboc1', 'deaboc2', 'sect_formal', 'sect_informal',
-        'sect_srvdom', 'sect_agricola', 'sub_inv', 'sub_informa',
+        'sect_srvdom', 'sect_moderno', 'sect_agricola', 'sub_inv', 'sub_informa',
         'ingrl', 'rama_act_1', 'rama_act_2', 'group_ocup_1', 'seguro',
         'migracion', )
     resource_class = Data_from_year_trimResource
