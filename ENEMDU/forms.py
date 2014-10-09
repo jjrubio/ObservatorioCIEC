944745bb56862788bec9c820809b595b554d9438
unknown <andreacaceresm@gmail.com>
2014-10-09 16:09:40 -0500

    Load data with files to db

---
+++ ENEMDU/forms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django import forms
+from django.conf import settings
+
+IMP_CHOICES = (('1', 'Datos 2003-4 a 2007-1'), ('2','Datos 2007-2 a la actualidad'))
+
+class UploadFileForm(forms.Form):
+	choices = forms.ChoiceField(label='Escoga que tabla desea cargar los datos', choices=IMP_CHOICES)
+	file = forms.FileField(label='Seleccione un archivo txt o csv')
--- ENEMDU/views.py
+++ ENEMDU/views.py
@@ -1,2 +1,42 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.shortcuts import HttpResponse, render_to_response
+from django.template.context import RequestContext
+from django.contrib import messages
+from models import *
+from ENEMDU.models import *
+from .forms import UploadFileForm
+import csv
+import os
+import subprocess
 
+def insert_data_enemdu(request):
+    context = RequestContext(request)
+    template = 'insert_data_enemdu.html'
+    upload_success = False
+
+    if request.method == 'POST':
+        upload_form = UploadFileForm(request.POST, request.FILES)
+
+        if upload_form.is_valid():
+            file = upload_form.cleaned_data['file']
+            choices = upload_form.cleaned_data['choices']
+            if choices == '1':
+                dbtable = 'ENEMDU_data_from_2003_4'
+            else:
+                dbtable = 'ENEMDU_data_from_2007_2'
+            new_file_import = upload_csv_file(upload=request.FILES['file'])
+            new_file_import.save()
+            #Luego de haber subido el archivo se corre el load_files.sh
+            #subprocess.call('/home/patu/Downloads/ObservatorioCIEC-master/load_files.sh')
+            #subprocess.call('/home/patu/Downloads/ObservatorioCIEC-master/load_files')
+            subprocess.Popen(['/home/patu/Downloads/ObservatorioCIEC-master/load_files',dbtable])
+            upload_success = True
+        else:
+            upload_form = UploadFileForm()
+            upload_success = False
+    else:
+        upload_form = UploadFileForm()
+        upload_success = False
+
+    return render_to_response(template, {'upload_form': upload_form, 'upload_success':upload_success}, context)
