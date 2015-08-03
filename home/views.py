ed3006139db77d48732906a4e4943b71ef6b0006
andreacaceres <andreacaceresm@gmail.com>
2015-08-03 16:04:08 -0500

    ip controller commented

--- home/views.py
+++ home/views.py
@@ -16,29 +16,29 @@ def slider(request):
     imagenes = Slider.objects.all()
     template = 'slider.html'
 
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    fecha_now = str(datetime.now())
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    get_ip = IP.objects.filter(ip=ip)
+    #x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    #fecha_now = str(datetime.now())
+    #if x_forwarded_for:
+    #    ip = x_forwarded_for.split(',')[0]
+    #else:
+    #    ip = request.META.get('REMOTE_ADDR')
+    #get_ip = IP.objects.filter(ip=ip)
 
-    get_value_counter = Counter.objects.all()
+    #get_value_counter = Counter.objects.all()
 
-    if not get_ip:
-        ip_save = IP(ip=ip, fecha = fecha_now)
-        ip_save.save()
+    #if not get_ip:
+    #    ip_save = IP(ip=ip, fecha = fecha_now)
+    #    ip_save.save()
         
-        new_counter = get_value_counter[0].counter + 1
-        save_new_counter = Counter.objects.update(counter=new_counter)
-    else:
-        if ( datetime.strptime(fecha_now[:-7], "%Y-%m-%d %H:%M:%S") -  datetime.strptime(get_ip[0].fecha[:-7], "%Y-%m-%d %H:%M:%S")).days > 0:
-            update_ip = IP.objects.filter(ip=get_ip[0].ip).update(fecha=fecha_now)
-            new_counter = get_value_counter[0].counter + 1
-            save_new_counter = Counter.objects.update(counter=new_counter)
+    #    new_counter = get_value_counter[0].counter + 1
+    #    save_new_counter = Counter.objects.update(counter=new_counter)
+    #else:
+    #    if ( datetime.strptime(fecha_now[:-7], "%Y-%m-%d %H:%M:%S") -  datetime.strptime(get_ip[0].fecha[:-7], "%Y-%m-%d %H:%M:%S")).days > 0:
+    #        update_ip = IP.objects.filter(ip=get_ip[0].ip).update(fecha=fecha_now)
+    #        new_counter = get_value_counter[0].counter + 1
+    #        save_new_counter = Counter.objects.update(counter=new_counter)
 
-    contador = Counter.objects.all()
+    #contador = Counter.objects.all()
     return render_to_response(template, context_instance = RequestContext(request,locals()))
 
 def map(request):
