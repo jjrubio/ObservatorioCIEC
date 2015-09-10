4d8a1d6ab59de194900c4982d1b58a677ab38c04
andreacaceres <andreacaceresm@gmail.com>
2015-09-10 16:14:22 -0500

    empty in values zeros

--- static/js/indicator.js
+++ static/js/indicator.js
@@ -364,7 +364,11 @@ function table(data){
                     if(data[i][2][j] == 0){
                         $('#periodo tr:last-child td:last-child').after('<td>   </td>');
                     }else{
-                        $('#periodo tr:last-child td:last-child').after('<td>'+data[i][3][j][k].toFixed(6)+'</td>');
+                        if (data[i][3][j][k].toFixed(0) == 0){
+                            $('#periodo tr:last-child td:last-child').after('<td>   </td>');  
+                        }else{
+                            $('#periodo tr:last-child td:last-child').after('<td>'+data[i][3][j][k].toFixed(6)+'</td>');
+                        }
                     }
                 }
             }
